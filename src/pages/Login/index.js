c06495bd7521c8efe0c44e3826aaeeb54723a076
Eliel Marques <elielrty@gmail.com>
2020-05-22 08:52:29 -0300

    implementando  a api e states

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,36 +1,111 @@
-import React from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, AsyncStorage, ActivityIndicator } from 'react-native';
 import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
-import {  useNavigation } from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
 import { LinearGradient } from "expo-linear-gradient";
 
 import styles from './style';
+import api from '../../services/api'
 
 export default function Login() {
   const navigation = useNavigation();
+  const passRef = useRef();
+  const [login, setLogin] = useState('');
+  const [pass, setPass] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState(null);
+  const [jwt, setJwt] = useState(null);
 
-  return(
+  async function saveUser(user) {
+    try {
+      await AsyncStorage.setItem('@CasaDosPobres:userToken', JSON.stringify(user))
+    } catch (e) {
+      alert("Erro ao salvar token" + e)
+    }
+  }
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('@CasaDosPobres:userToken')
+      if (value !== null) {
+        setJwt(value)
+      }
+    } catch (e) {
+      alert(e)
+    }
+  }
+
+  const authenticate = async () => {
+    if (login.length === 0 || pass.length === 0) {
+      alert("Preencha os campos Vazios")
+      return
+    }
+
+    setLoading(true);
+    try {
+      const credentials = {
+        cpf: login,
+        senha: pass
+      };
+      const response = await api.post('doador/login', credentials);
+
+      setUser(response.data);
+
+      await saveUser(user)  // salvar dados no AsyncStorage
+
+      // const resetAction = StackActions.reset({
+      //   index: 0,
+      //   actions: [NavigationActions.navigate({ routeName: 'App' })],
+      // })
+
+      setLoading(false);
+
+      // props.navigation.dispatch(resetAction)
+    } catch (e) {
+      console.log(e);
+      let error = e.response.data.error;
+      alert(error + " tente novamente")
+      setLoading(false);
+    }
+  }
+
+  return (
     <View style={styles.container}>
-        <Text style={styles.header}>Entrar</Text>
+      <Text style={styles.header}>Entrar</Text>
       <View style={styles.form}>
-        <TextInput style={styles.input} placeholder="CPF" placeholderTextColor="#999999" />
+        <TextInput style={styles.input}
+          placeholder="CPF"
+          placeholderTextColor="#999999"
+          keyboardType="numeric"
+          returnKeyType="next"
+          maxLength={11}
+          onChangeText={(cpf) => setLogin(cpf)}
+          onSubmitEditing={() => passRef.current.focus()} />
         <MaterialCommunityIcons style={styles.formIcon} name="file-document-box-outline" size={24} color="#999999" />
       </View>
       <View style={styles.form}>
-        <TextInput style={styles.input} placeholder="Senha" placeholderTextColor="#999999" secureTextEntry={true} />
+        <TextInput style={styles.input}
+          placeholder="Senha"
+          placeholderTextColor="#999999"
+          secureTextEntry={true}
+          ref={passRef}
+          autoCapitalize="none"
+          onChangeText={(pass) => setPass(pass)}
+          onSubmitEditing={() => authenticate()} />
         <MaterialIcons style={styles.formIcon} name="lock" size={24} color="#999999" />
       </View>
       <TouchableOpacity onPress={() => navigation.navigate('Forgot')}>
         <Text style={styles.forgot}>Esqueceu a senha?</Text>
       </TouchableOpacity>
       <View style={styles.form}>
-        <TouchableOpacity style={styles.submitButtom} onPress={() => {}}>
-          <LinearGradient colors={['#81bd3c', '#629648', '#106b34']} start={[1.1, 1.9]} end={[0.2 , 1.1]} style={styles.submit} >
-            <Text style={styles.submitText}>Entrar</Text>
+        <TouchableOpacity style={styles.submitButtom}
+          onPress={() => authenticate()}>
+          <LinearGradient colors={['#81bd3c', '#629648', '#106b34']} start={[1.1, 1.9]} end={[0.2, 1.1]} style={styles.submit} >
+            {loading ? <ActivityIndicator size="small" color="#fff" />
+              : <Text style={styles.submitText}>Entrar</Text>}
           </LinearGradient >
         </TouchableOpacity>
         <TouchableOpacity style={styles.register} onPress={() => navigation.navigate('Register')}>
-            <Text style={styles.registerText}>Não tem conta? <Text style={styles.link}>Cadastre-se</Text></Text>
+          <Text style={styles.registerText}>Não tem conta? <Text style={styles.link}>Cadastre-se</Text></Text>
         </TouchableOpacity>
       </View>
     </View>
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,37 +1,117 @@
-import React from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, ActivityIndicator } from 'react-native';
 import { MaterialCommunityIcons, MaterialIcons, FontAwesome } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import { LinearGradient } from "expo-linear-gradient";
 
 import styles from './style';
+import api from '../../services/api'
 
 export default function Register() {
   const navigation = useNavigation();
+  const emailRef = useRef();
+  const cpfRef = useRef();
+  const passRef = useRef();
+  const [ name, setName ] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ cpf, setCpf ] = useState('');
+  const [ pass, setPass ] = useState('');
+  const [ loading, setLoading] = useState(false);
+  const [ resposta, setResposta] = useState('')
+
+  const authenticate = async () => {
+    if (name.length === 0 || email.length === 0 || cpf.length === 0 || pass.length === 0) {
+      alert("Preencha os campos Vazios")
+      return
+    }
+
+    setLoading(true);
+    try {
+      const credentials = {
+        nome: name, 
+        email: email, 
+        senha: pass, 
+        cpf: cpf
+      };
+      const response = await api.post('doador/new', credentials);
+
+      // setResposta(response.data);
+      alert(JSON.stringify(response.data.success))
+      //await saveUser(user)  // salvar dados no AsyncStorage
+
+      // const resetAction = StackActions.reset({
+      //   index: 0,
+      //   actions: [NavigationActions.navigate({ routeName: 'App' })],
+      // })
+
+      setLoading(false);
+
+      // props.navigation.dispatch(resetAction)
+    } catch (e) {
+      console.log(e);
+      
+      alert(e + " tente novamente")
+      setLoading(false);
+    }
+  }
 
   return(
     <View style={styles.container}>
       <Text style={styles.header}>Cadastro</Text>
       <View style={styles.form}>
-        <TextInput style={styles.input} placeholder="Nome" placeholderTextColor="#999999" />
+        <TextInput style={styles.input} 
+                   placeholder="Nome" 
+                   placeholderTextColor="#999999"
+                   autoCapitalize="words"
+                   returnKeyType="next"
+                   onChangeText={(name) => setName(name)} 
+                   onSubmitEditing={() => emailRef.current.focus()}
+                   />
         <FontAwesome style={styles.formIcon} name="user" size={24} color="#999999" />
       </View>
       <View style={styles.form}>
-        <TextInput style={styles.input} placeholder="Email" placeholderTextColor="#999999" />
+        <TextInput style={styles.input}
+                   placeholder="Email" 
+                   placeholderTextColor="#999999"
+                   autoCapitalize="none"
+                   keyboardType="email-address" 
+                   returnKeyType="next"
+                   ref={emailRef}
+                   onChangeText={(email) => setEmail(email)} 
+                   onSubmitEditing={() => cpfRef.current.focus()}
+                   />
         <MaterialCommunityIcons style={styles.formIcon} name="email" size={24} color="#999999" /> 
       </View>
       <View style={styles.form}>
-        <TextInput style={styles.input} placeholder="CPF" placeholderTextColor="#999999" />
+        <TextInput style={styles.input} 
+                   placeholder="CPF" 
+                   placeholderTextColor="#999999"
+                   keyboardType="numeric" 
+                   returnKeyType="next"
+                   maxLength={11}
+                   ref={cpfRef}
+                   onChangeText={(cpf) => setCpf(cpf)} 
+                   onSubmitEditing={() => passRef.current.focus()}
+                   />
         <MaterialCommunityIcons style={styles.formIcon} name="file-document-box-outline" size={24} color="#999999" />
       </View>
       <View style={styles.form}>
-        <TextInput style={styles.input} placeholder="Senha" placeholderTextColor="#999999" secureTextEntry={true} />
+        <TextInput style={styles.input} 
+                   placeholder="Senha" 
+                   placeholderTextColor="#999999" 
+                   secureTextEntry={true}
+                   returnKeyType="send"
+                   ref={passRef}
+                   onChangeText={(pass) => setPass(pass)} 
+                   onSubmitEditing={() => authenticate() }
+                   />
         <MaterialIcons style={styles.formIcon} name="lock" size={24} color="#999999" />
       </View>
       <View style={styles.form}>
-        <TouchableOpacity style={styles.submitButtom} onPress={() => {}}>
+        <TouchableOpacity style={styles.submitButtom} onPress={() => authenticate()}>
           <LinearGradient colors={['#81bd3c', '#629648', '#106b34']} start={[1.1, 1.9]} end={[0.2 , 1.1]} style={styles.submit} >
-            <Text style={styles.submitText}>Entrar</Text>
+          {loading ? <ActivityIndicator size="small" color="#fff" />
+              : <Text style={styles.submitText}>Entrar</Text>}
           </LinearGradient >
         </TouchableOpacity>
         <TouchableOpacity style={styles.register} onPress={() => navigation.navigate('Login')}>
