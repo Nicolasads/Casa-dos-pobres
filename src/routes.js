e88ec1ba0606c31d2527dbb44a2f13a7ec5c520d
eliel marques carvalho <elielrty@gmail.com>
2020-06-03 17:53:37 -0300

    correção

--- src/routes.js
+++ src/routes.js
@@ -32,7 +32,7 @@ export default () =>  {
           const response = await api.get('doador/check/login')
           if(response.data.success){
              setJwt(token)
-            setLogged(false)
+            setLogged(true)
           }
         }
        } catch (e) {
